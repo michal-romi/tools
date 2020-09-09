c0336beaabbfc0c6645c38d8885b671867a8603d
michal-romi <michal-romi@users.noreply.github.com>
2020-09-09 09:47:41 +0300

    Add files via upload

--- download_images_from_file.py
+++ download_images_from_file.py
@@ -1,14 +1,29 @@
 import urllib.request
 from pathlib import Path
+import os
+import re
+
+input_file = input("Enter input file name with no header: ")
+temp = re.split(r'(\\|\/)',input_file)[:-1]
+path = ""
+for i in temp:
+    path = path+i
+
 
 lines = []
 # Make sure to change the path to the file you want to read from
-with open("C:\\Temp\\other\\other.csv") as file:
-    for line in file:
-        line = line.strip()  # remove spaces / special characters
-        lines.append(line)  # for storing in memory which can help with larger files
-        # make sure to updatethe location to which the images will be saved
-        # takes the last part of the url as the new file name
-        filename = Path("C:\\Temp\\other\\" + line.split('/')[-1])
-        urllib.request.urlretrieve(line, filename)
+if os.path.exists(input_file):
+    with open(input_file) as file:
+        for line in file:
+            line = line.strip()  # remove spaces / special characters
+            lines.append(line)  # for storing in memory which can help with larger files
+            # make sure to updatethe location to which the images will be saved
+            # takes the last part of the url as the new file name
+            filename = Path(path + line.split('/')[-1])
+            try:
+                urllib.request.urlretrieve(line, filename)
+            except:
+                print("couldn't open file {}".format(filename) )
 
+else:
+    print("File doesn't exist")
--- shuffle_and_split_csv.py
+++ shuffle_and_split_csv.py
@@ -1,13 +1,33 @@
-# read a csv file without header and n files to split and then shuffle the file and split it into n smaller files
-# Make sure to update the input_file, output_path and number of files you want
+""" read a csv file without header and n files to split and then shuffle the file and split it into n smaller files
+ Make sure to update the input_file, output_path and number of files you want
+"""
 import pandas as pd
-
-input_file = 'c:\Temp\explicit_conf_final.csv'
-num_of_files_to_split = 3
-output_path = 'c:/Temp/'
+import os
+import sys
 
 
-df = pd.read_csv(input_file,header=None)
+input_file = input("Enter file name to split: ")
+num_of_files_to_split = input("Enter number of files to split to: ")
+output_path = input("Enter output directory: ")
+
+if os.path.exists(input_file):
+    df = pd.read_csv(input_file,header=None)
+else:
+    print("Input file doesn't exist")
+    sys.exit()
+
+try:
+    num_of_files_to_split = int(num_of_files_to_split)
+except:
+    print("Number of files to split into is not a number")
+    sys.exit()
+
+if os.path.exists(output_path):
+    print("Start processing")
+else:
+    print("Output directory doesn't exist")
+    sys.exit()
+
 shuffled_df = df.sample(frac=1, random_state=1).reset_index(drop=True)
 rows = shuffled_df.size
 num_per_df = round(rows / num_of_files_to_split)
@@ -28,5 +48,5 @@ for i in range(num_of_files_to_split):
         df_names[i] = shuffled_df[start_index:]
 
 for i in range(num_of_files_to_split):
-    file_name = output_path + str(i) + '.csv'
+    file_name = output_path + '/' + str(i) + '.csv'
     df_names[i].to_csv(file_name)
